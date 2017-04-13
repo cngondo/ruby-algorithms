ba481664f9723d249bb8159c3734f03378f1b737
cngondo <devngondo@gmail.com>
2017-04-14 00:17:04 +0300

    worked with select method

--- select.rb
+++ select.rb
@@ -1,2 +1,12 @@
 # Given an array of strings
 # return strings with over 5 letters
+# arr = %w(The quick brown fox jumped over the lazy dog. I was flabberghasted!)
+#
+# p arr.select{ |x| x.length > 5}
+
+
+# Given an array of strings
+# return all of the vowels
+
+# Passed in the block a regex builder using the "~" operator
+p %w(a b c d e f u q e).select { |v| v =~ /[aeiou]/ }
